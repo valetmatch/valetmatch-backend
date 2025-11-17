9e29394d97458618d83e0f0b97245c72c65ccee8
valetmatch <deputymitchell@me.com>
2025-11-17 15:34:57 +0000

    Add trust proxy setting for Render

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', true);
 const PORT = process.env.PORT || 3001;
 
 // Middleware
